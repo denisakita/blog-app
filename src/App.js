df882480f65d5dca36e61e8060259a4ef4354c5b
denisakita <78655648+denisakita@users.noreply.github.com>
2024-04-02 15:02:31 +0200

    Set up react (#1)
    * install react router dom and create components
    
    * add nav bar
    
    * create article page
    
    * use reusable components
    
    * complete other components
    
    * install axios
    
    * add a comment form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {AboutPage, ArticlePage, ArticlesListPage, HomePage, NotFoundPage} from "./pages";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {NavBar} from "./components";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <NavBar/>
+                <div id="page-body">
+                    <Routes>
+                        <Route path="/" element={<HomePage/>}/>
+                        <Route path="/about" element={<AboutPage/>}/>
+                        <Route path="/articles" element={<ArticlesListPage/>}/>
+                        <Route path="/articles/:articleId" element={<ArticlePage/>}/>
+                        <Route path="*" element={<NotFoundPage/>}/>
+
+                    </Routes>
+                </div>
+            </div>
+
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/pages/AboutPage.js
@@ -0,0 +1,25 @@
+const AboutPage = () => {
+    return (
+        <>
+            <h1>About Me</h1>
+            <p>
+                Welcome to our About page! We're a passionate team of creators dedicated to sharing valuable content and
+                insights with our audience.
+            </p>
+            <p>
+                Our mission is to provide high-quality articles, tutorials, and resources covering a wide range of
+                topics, including technology, programming, personal development, and more.
+            </p>
+            <p>
+                We believe in the power of knowledge and continuous learning. Through our blog, we aim to inspire,
+                educate, and empower individuals to pursue their passions and achieve their goals.
+            </p>
+            <p>
+                Thank you for taking the time to learn more about us. We're excited to have you join our community and
+                embark on this journey of growth and discovery together!
+            </p>
+        </>
+    );
+}
+
+export default AboutPage;
