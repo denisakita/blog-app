5923c5882fd191812e06456b70f715c52f0cbcac
Denisa Kita <denisakita7@gmail.com>
2024-04-02 14:07:06 +0200

    install axios

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export {default as ArticleList} from './ArticleList';
 export {default as NavBar} from './NavBar';
+export {default as CommentsList} from './CommentsList';
--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,9 +1,25 @@
 import {useParams} from 'react-router-dom';
 import articles from '../mock/article-content';
 import {NotFoundPage} from "./index";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import {CommentsList} from "../components";
 
 const ArticlePage = () => {
+    const [articleInfo, setArticleInfo] = useState({upvotes: 0, comments: []});
     const {articleId} = useParams();
+
+
+    useEffect(() => {
+        const loadArticleInfo = async () => {
+            const response = await axios.get(`/api/articles/${articleId}`)
+            const newArticleInfo = response.data;
+            setArticleInfo(newArticleInfo)
+        }
+        loadArticleInfo();
+
+    }, [articleId]);
+
     const article = articles.find(article => article.name === articleId);
 
     if (!article) {
@@ -12,9 +28,11 @@ const ArticlePage = () => {
     return (
         <>
             <h1>{article.title}</h1>
+            <p>This article has {articleInfo.upvotes} upvote(s)</p>
             {article.content.map((paragraph, index) => (
                 <p key={index}>{paragraph}</p>
             ))}
+            <CommentsList comments={articleInfo.comments}/>
         </>
     );
 }
