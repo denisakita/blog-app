a92ab466ac1a8f06e2c4812979fb539d94254cab
Denisa Kita <denisakita7@gmail.com>
2024-04-03 08:44:17 +0200

    refactor

--- src/components/AddCommentForm.js
+++ src/components/AddCommentForm.js
@@ -1,22 +1,25 @@
-import {useState} from "react";
-import axios from "axios";
+import {useState} from 'react';
+import axios from 'axios';
+import useUser from '../hooks/useUser';
 
-const AddCommentForm = ({articleName,onArticleUpdated}) => {
-
+const AddCommentForm = ({articleName, onArticleUpdated}) => {
     const [name, setName] = useState('');
     const [commentText, setCommentText] = useState('');
+    const {user} = useUser();
 
     const addComment = async () => {
+        const token = user && await user.getIdToken();
+        const headers = token ? {authtoken: token} : {};
         const response = await axios.post(`/api/articles/${articleName}/comments`, {
             postedBy: name,
-            text: commentText
+            text: commentText,
+        }, {
+            headers,
         });
         const updatedArticle = response.data;
         onArticleUpdated(updatedArticle);
         setName('');
         setCommentText('');
-
-
     }
 
     return (
@@ -34,10 +37,12 @@ const AddCommentForm = ({articleName,onArticleUpdated}) => {
                 <textarea
                     value={commentText}
                     onChange={e => setCommentText(e.target.value)}
-                    rows="4" cols="50"/>
+                    rows="4"
+                    cols="50"/>
             </label>
             <button onClick={addComment}>Add Comment</button>
         </div>
     )
 }
+
 export default AddCommentForm;
--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,10 +1,11 @@
-import {useParams} from 'react-router-dom';
-import articles from '../mock/article-content';
-import {NotFoundPage} from "./index";
+import {useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import axios from "axios";
+import {NotFoundPage} from "./index";
 import {AddCommentForm, CommentsList} from "../components";
 import useUser from "../hooks/useUser";
+import articles from "../mock/article-content";
+
 
 const ArticlePage = () => {
     const [articleInfo, setArticleInfo] = useState({upvotes: 0, comments: []});
@@ -12,21 +13,24 @@ const ArticlePage = () => {
 
     const {user, isLoading} = useUser();
 
-
     useEffect(() => {
         const loadArticleInfo = async () => {
-            const response = await axios.get(`/api/articles/${articleId}`)
+            const token = user && await user.getIdToken();
+            const headers = token ? {authtoken: token} : {};
+            const response = await axios.get(`/api/articles/${articleId}`, {headers});
             const newArticleInfo = response.data;
-            setArticleInfo(newArticleInfo)
+            setArticleInfo(newArticleInfo);
         }
-        loadArticleInfo();
 
-    }, [articleId]);
+        loadArticleInfo();
+    }, []);
 
     const article = articles.find(article => article.name === articleId);
 
-    const addUpVote = async () => {
-        const response = await axios.put(`/api/articles/${articleId}/upvote`);
+    const addUpvote = async () => {
+        const token = user && await user.getIdToken();
+        const headers = token ? {authtoken: token} : {};
+        const response = await axios.put(`/api/articles/${articleId}/upvote`, null, {headers});
         const updatedArticle = response.data;
         setArticleInfo(updatedArticle);
     }
@@ -34,27 +38,24 @@ const ArticlePage = () => {
     if (!article) {
         return <NotFoundPage/>
     }
+
     return (
         <>
             <h1>{article.title}</h1>
             <div className="upvotes-section">
                 {user
-                    ? <button onClick={addUpVote}>UpVote</button>
-                    : <button>Login to upvote</button>}
+                    ? <button onClick={addUpvote}>Upvote</button>
+                    : <button>Log in to upvote</button>}
                 <p>This article has {articleInfo.upvotes} upvote(s)</p>
             </div>
-
-            {article.content.map((paragraph, index) => (
-                <p key={index}>{paragraph}</p>
+            {article.content.map((paragraph, i) => (
+                <p key={i}>{paragraph}</p>
             ))}
             {user
                 ? <AddCommentForm
                     articleName={articleId}
-                    onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)}
-                />
-
-                : <button>Login to add a comment</button>
-            }
+                    onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)}/>
+                : <button>Log in to add a comment</button>}
             <CommentsList comments={articleInfo.comments}/>
         </>
     );
